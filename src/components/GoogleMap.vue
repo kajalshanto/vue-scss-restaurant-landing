a1232bfe1baa91088dcc742450ca5650a41cda67
Hasibul Hasan Shanto <hasibulhasan.kajal@gmail.com>
2020-08-24 15:29:42 +0600

    update maps and menus

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,7 +1,6 @@
 <template>
 	<div id="gmap">
-        
-		<div class="container text-center overflow-hidden pt-100"> 
+		<div class="container text-center overflow-hidden pt-100">
 			<!-- row  -->
 			<div class="row">
 				<!-- Section title  -->
@@ -21,7 +20,16 @@
 			<!--/. row  -->
 			<div class="row">
 				<div class="col-sm-12 pt-100 mb-100">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12274.249070817985!2d90.409553718907!3d23.84859072018039!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c68497f240fd%3A0x1656f8935ef85f50!2sAirport%20Restaurant!5e0!3m2!1sen!2sbd!4v1598224426902!5m2!1sen!2sbd" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+					<iframe
+						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12274.249070817985!2d90.409553718907!3d23.84859072018039!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c68497f240fd%3A0x1656f8935ef85f50!2sAirport%20Restaurant!5e0!3m2!1sen!2sbd!4v1598224426902!5m2!1sen!2sbd"
+						width="100%"
+						height="500"
+						frameborder="0"
+						style="border:0;"
+						allowfullscreen=""
+						aria-hidden="false"
+						tabindex="0"
+					></iframe>
 					<!--/. Google Map Embeded  -->
 				</div>
 			</div>
@@ -31,17 +39,13 @@
 
 <script>
 	export default {
-		name: "Gmap"
+		name: "Gmap",
 	};
 </script>
 
 <style>
-    .clearfix{
-        clear:both;
-        height: auto;
-    }
-    #gmap{  
-		top: auto; 
-		background-color:whitesmoke; 
-    }
+	#gmap {
+		top: auto;
+		background-color: whitesmoke;
+	}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,54 +1,44 @@
 <template>
-    <div id="home">
-        <Header/>
-        <Hero/>
-        <About/>
-        <Dishes/> 
-        <Menu/>
-        <Gallery/>
-        <Reservation/>
-        <Gmap/>
-        <Testimonials/>
-        <Footer/>
-
-    </div>
+	<div id="home">
+		<Header />
+		<Hero />
+		<About />
+		<Dishes />
+		<Menu />
+		<Gallery />
+		<Reservation />
+		<Gmap />
+		<Testimonials />
+		<Footer />
+	</div>
 </template>
 
 <script>
-import Header from './Header'
-import Hero from './Hero'
-import About from './About'
-import Dishes from './Dishes'
-import Menu from './FoodMenu'
-import Gallery from './Gallery'
-import Reservation from './Reservation'
-import Gmap from './GoogleMap'
-import Testimonials from './Testimonials'
-import Footer from './Footer'
+	import Header from "./Header";
+	import Hero from "./Hero";
+	import About from "./About";
+	import Dishes from "./Dishes";
+	import Menu from "./FoodMenu";
+	import Gallery from "./Gallery";
+	import Reservation from "./Reservation";
+	import Gmap from "./GoogleMap";
+	import Testimonials from "./Testimonials";
+	import Footer from "./Footer";
 
+	export default {
+		name: "Home",
 
-
-
-
-
-
-
-    export default {
-        name: 'Home',
-
-        components: {
-            Header, 
-            Hero,
-            About,
-            Dishes,
-            Menu,
-            Gallery,
-            Reservation,            
-            Gmap,
-            Testimonials,
-            Footer
-        }
-    }
+		components: {
+			Header,
+			Hero,
+			About,
+			Dishes,
+			Menu,
+			Gallery,
+			Reservation,
+			Gmap,
+			Testimonials,
+			Footer,
+		},
+	};
 </script>
-
- 
