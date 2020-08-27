4e3778f2c832110950733da91967c6d43878049f
Hasibul Hasan Shanto <hasibulhasan.kajal@gmail.com>
2020-08-27 14:26:11 +0600

    update button style and position

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -26,7 +26,7 @@
 	import Testimonials from "./Testimonials";
 	import Footer from "./Footer";
 
-	import GoTop from '@inotom/vue-go-top';
+	import GoTop from "@inotom/vue-go-top";
 	export default {
 		name: "Home",
 
@@ -41,7 +41,27 @@
 			Gmap,
 			Testimonials,
 			Footer,
-			GoTop
+			GoTop,
 		},
 	};
 </script>
+
+<style scoped>
+	.vue-go-top {
+		width: 70px;
+		height: 70px; 
+		right: 35px !important; /* deafult 50px */
+		bottom: calc(100px);
+		z-index: 1000;
+		color: rgb(255, 255, 255);
+		background-color: rgb(255 0 0 / 1) !important;
+		/* box-shadow: #343a40 1px 1px 2px !important; */
+		box-shadow: #000 1px 1px 2px !important;
+		border-radius: 50%;
+	}
+	.vue-go-top:focus,
+	.vue-go-top:active {
+		outline: none !important; 
+		box-shadow: none !important;
+	}
+</style>
